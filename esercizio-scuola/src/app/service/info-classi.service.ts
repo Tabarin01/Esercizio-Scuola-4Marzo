c8dd3003d910c347b3f3db7013e4b0b5ac6398da
Tabarin01 <119614157+Tabarin01@users.noreply.github.com>
2024-03-05 00:34:52 +0100

    Co-authored-by: Simone Michelon <SimoneMichelon@users.noreply.github.com>

--- esercizio-scuola/src/app/service/info-classi.service.ts
+++ esercizio-scuola/src/app/service/info-classi.service.ts
@@ -11,37 +11,37 @@ export class InfoClassiService {
     rappresentante: string;
     mascotte: string;
   }[] = [
-    // {
-    //   classe: 4,
-    //   sezione: 'C',
-    //   laboratorio: true,
-    //   rappresentante: 'Luna Stardust',
-    //   mascotte:
-    //     'https://ih1.redbubble.net/image.4909511332.3792/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-    // },
-    // {
-    //   classe: 2,
-    //   sezione: 'D',
-    //   laboratorio: false,
-    //   rappresentante: 'Neo Flux',
-    //   mascotte:
-    //     'https://cdn.corrieredellosport.it/images/2020/05/17/132405093-9c63a25a-4276-4882-b454-a517574022bf.jpeg',
-    // },
-    // {
-    //   classe: 5,
-    //   sezione: 'B',
-    //   laboratorio: true,
-    //   rappresentante: 'Astra Nova',
-    //   mascotte: 'https://i.ebayimg.com/images/g/Nh0AAOSwopljvN4H/s-l1200.webp',
-    // },
-    // {
-    //   classe: 1,
-    //   sezione: 'E',
-    //   laboratorio: false,
-    //   rappresentante: 'Cosmo Spark',
-    //   mascotte:
-    //     'https://c4.wallpaperflare.com/wallpaper/937/112/903/adults-riley-reid-brunette-face-wallpaper-preview.jpg',
-    // },
+    {
+      classe: 4,
+      sezione: 'C',
+      laboratorio: true,
+      rappresentante: 'Luna Stardust',
+      mascotte:
+        'https://ih1.redbubble.net/image.4909511332.3792/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+    },
+    {
+      classe: 2,
+      sezione: 'D',
+      laboratorio: false,
+      rappresentante: 'Neo Flux',
+      mascotte:
+        'https://cdn.corrieredellosport.it/images/2020/05/17/132405093-9c63a25a-4276-4882-b454-a517574022bf.jpeg',
+    },
+    {
+      classe: 5,
+      sezione: 'B',
+      laboratorio: true,
+      rappresentante: 'Astra Nova',
+      mascotte: 'https://i.ebayimg.com/images/g/Nh0AAOSwopljvN4H/s-l1200.webp',
+    },
+    {
+      classe: 1,
+      sezione: 'E',
+      laboratorio: false,
+      rappresentante: 'Cosmo Spark',
+      mascotte:
+        'https://c4.wallpaperflare.com/wallpaper/937/112/903/adults-riley-reid-brunette-face-wallpaper-preview.jpg',
+    },
     {
       classe: 2,
       sezione: 'A',
@@ -55,8 +55,7 @@ export class InfoClassiService {
       sezione: 'A',
       laboratorio: true,
       rappresentante: 'Mario Fringuelli',
-      mascotte:
-        'https://www.yardbarker.com/media/4/4/4430df731e3628f5922a6a04acc82c276b975235/thumb_16x9/8977515.jpg',
+      mascotte: 'https://i.ebayimg.com/images/g/br0AAOSwg69hbAPx/s-l1200.webp',
     },
     {
       classe: 3,
@@ -64,7 +63,7 @@ export class InfoClassiService {
       laboratorio: true,
       rappresentante: 'Andrea Scarrone',
       mascotte:
-        'https://img.bleacherreport.net/img/slides/photos/003/901/874/hi-res-0a10a21f5a96924445613bbef74d0759_crop_exact.jpg?w=2975&h=2048&q=85',
+        'https://i.dailymail.co.uk/1s/2023/01/03/15/66151061-11595191-image-m-225_1672761020998.jpg',
     },
     {
       classe: 5,
@@ -72,7 +71,7 @@ export class InfoClassiService {
       laboratorio: true,
       rappresentante: 'Micheal Jordan',
       mascotte:
-        'https://images.squarespace-cdn.com/content/v1/5c6463897fdcb8d3e8cf7d9c/1560817385865-A86N711HU2I17MV9NY9S/benny-the-bull.jpg',
+        'https://www.redbled.com/wp-content/uploads/2023/11/brazzers-pornstars-1200x720.jpg',
     },
   ];
 
